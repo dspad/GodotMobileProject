5560fc4a3531d78f8979d2aa41994d0151a21f38
Daniele Spadoni <d.spadoni@infordata.net>
2025-02-27 10:03:36 +0100

    Setup time left timer

--- main.gd
+++ main.gd
@@ -1,8 +1,21 @@
 extends Control
 
 var score : int = 0
+@export var time_left : int = 5
+
+func _ready() -> void:
+	$TimeLeftLabel.text = "Time left: 0" + str(time_left) + "s"
 
 func _on_increase_score_button_pressed() -> void:
 	score += 1
 	$ScoreLabel.text = "Score: " + str(score)
 	Input.vibrate_handheld(125)
+
+
+func _on_time_left_timer_timeout() -> void:
+	time_left -= 1
+	if time_left > 0:
+		$TimeLeftLabel.text = "Time left: 0" + str(time_left) + "s"
+	else:
+		print("Game Over")
+		$TimeLeftTimer.stop()
