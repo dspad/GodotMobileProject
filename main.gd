dfe9e153fb8a193221420c2db55a53254d1ed49f
Daniele Spadoni <d.spadoni@infordata.net>
2025-02-27 10:26:44 +0100

    Win scene login

--- main.gd
+++ main.gd
@@ -1,15 +1,19 @@
 extends Control
 
 var score : int = 0
+@export var aim_score : int = 10
 @export var time_left : int = 5
 
 func _ready() -> void:
 	$TimeLeftLabel.text = "Time left: 0" + str(time_left) + "s"
+	$AimScoreLabel.text = "Aim score: " + str(aim_score)
 
 func _on_increase_score_button_pressed() -> void:
 	score += 1
 	$ScoreLabel.text = "Score: " + str(score)
 	Input.vibrate_handheld(125)
+	if score == aim_score:
+		get_tree().change_scene_to_file("res://Win.tscn")
 
 
 func _on_time_left_timer_timeout() -> void:
